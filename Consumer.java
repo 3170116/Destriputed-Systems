2f8ba2afbdd7007590f0383076bfca12835888d5
dimitris_bek <p3170116@gmail.com>
2020-03-11 18:06:05 +0200

    Έβαλα το socket

--- Consumer.java
+++ Consumer.java
@@ -1,9 +1,51 @@
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 class Consumer {
 
+    private Thread t;
+
+    private Socket requestSocket = null;
+    private ObjectOutputStream out = null;
+    private ObjectInputStream in = null;
+
     public Consumer() {
+        t = new Thread() {
+            @Override
+            public void run() {
+                try {
+
+                    requestSocket = new Socket("127.0.0.1", 5432);
+                    out = new ObjectOutputStream(requestSocket.getOutputStream());
+                    in = new ObjectInputStream(requestSocket.getInputStream());
+
+                } catch (UnknownHostException unknownHost) {
+                    System.err.println("You are trying to connect to an unknown host!");
+                } catch (IOException ioException) {
+                    ioException.printStackTrace();
+                } finally {
+                    try {
+                        in.close(); out.close();
+                        requestSocket.close();
+                    } catch (IOException ioException) {
+                        ioException.printStackTrace();
+                    }
+                }
+            }
+        };
     }
 
-    public void register(Broker broker, ArtistName artname) {}
+    public void register(Broker broker, ArtistName artname) {
+        t.start();
+    }
+
     public void disconnect(Broker broker, ArtistName artname) {}
     public void playData (ArtistName artname, Value value) {}
+
+    public static void main(String[] args) {
+        new Consumer().register(null,null);
+    }
 }
--- Publisher.java
+++ Publisher.java
@@ -1,11 +1,57 @@
-class Publisher {
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+class Publisher implements Comparable<Publisher> {
+
+    private String ipAddress;
+    private int port;
+
+    private ServerSocket providerSocket;
+    private Socket connection = null;
 
     public Publisher() {
+        this.ipAddress = "127.0.0.1";
+        this.port = 4321;
+
+        try {
+            providerSocket = new ServerSocket(this.port, 10);
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
     }
-	
-    
+
+    public void accept() {
+        while (true) {
+            try {
+                connection = providerSocket.accept();
+                System.out.println("Broker " + connection + "connected");
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                /*try {
+                    providerSocket.close();
+                } catch (IOException ioException) {
+                    ioException.printStackTrace();
+                }*/
+            }
+        }
+    }
+
     public void getBrokerList() {}
     public Broker hashTopic(ArtistName artname) {return null;}
     public void push(ArtistName artname, Value value) {}
     public void notifyFailure(Broker broker) {}
+
+    public static void main(String[] args) {
+        Publisher publisher = new Publisher();
+        publisher.accept();
+    }
+
+    @Override
+    public int compareTo(Publisher p) {
+        if (this.ipAddress.equals(p.ipAddress) && this.port == p.port)
+            return 0;
+        return -1;
+    }
 }
